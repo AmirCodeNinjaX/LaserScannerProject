e5a7b13e7e8674b45af606862a90c111de5191d0
AmirCodeNinjaX <abolhasaniir1356@gmail.com>
2026-08-02 18:09:19 +0330

    Compatiblizing the code for Raspberry PI Cam

--- Image_Processing_Code/03_Camera_Calibration_With_Chessboard_By_Camera.py
+++ Image_Processing_Code/03_Camera_Calibration_With_Chessboard_By_Camera.py
@@ -1,81 +1,68 @@
 import numpy as np
 import cv2 as cv
-import glob
-import os
-import matplotlib.pyplot as plt
+import ast
+from picamera2 import Picamera2
+from Additional._00_Camera_Calibration_Modules import load_camera_config, CONFIG_PATH
 
-def calibrate(ShowPics=True,Camera_Index=0):
-    #ReadImage
-    root = os.getcwd()
-    calibrationDir = os.path.join(root,"ChessImages")
-    imagePathlist = glob.glob(os.path.join(calibrationDir,'*.jpg'))
-
+def calibrate():
     #Initialize
     nRows = 6
     nCols = 8
-    termCriteria = (cv.TERM_CRITERIA_EPS+cv.TERM_CRITERIA_MAX_ITER,30,0.001)
+    
     worldPtsCur = np.zeros((nRows*nCols,3),np.float32)
     worldPtsCur[:,:2] = np.mgrid[0:nRows,0:nCols].T.reshape(-1,2)
-    worldPtsList = []
-    imgPtsList = []
 
-    #FindCorners
-    # for curImgPath in imagePathlist:
-    #     print(curImgPath)
-    
-    cam = cv.VideoCapture(Camera_Index)
+    configs = load_camera_config(CONFIG_PATH)
+
+    picam2 = Picamera2()
+    config = picam2.create_preview_configuration(
+    main={"size": (480, 320), "format": "RGB888"}
+)
+    picam2.configure(config)
+    picam2.start()
+
+    exposure = int(configs["exposure"])
+    gain_x10 = float(configs["gain"])
+    initial_colour_gains = configs["initial_colour_gains"]
+
+    a, b = map(float, initial_colour_gains.strip("()").split(","))
+
+    initial_colour_gains = (float(a),float(b))
+
+    print(a,b)
+
+    exposure = max(exposure, 1)
+    gain_x10 = max(gain_x10, 1)
+    gain = gain_x10 / 10.0
+
+    picam2.set_controls({
+        "AeEnable": False,
+        "AwbEnable": False,
+        "ExposureTime": exposure,
+        "AnalogueGain": gain,
+        "ColourGains": initial_colour_gains
+    })
+
+
     img_index = 0
     while True:
-        ret, imgBGR = cam.read()
-        if(not(ret)): continue
+        imgBGR = picam2.capture_array()
+        
         cv.imshow('Camera',imgBGR)
         imgGray = cv.cvtColor(imgBGR,cv.COLOR_BGR2GRAY)
-        # cv.imshow('bgr image',imgBGR)
-        # cv.imshow('grayscale image',imgGray)
-        cornersFound, cornersOrg = cv.findChessboardCorners(imgGray,(6,7),None)
+        cornersFound, _ = cv.findChessboardCorners(imgGray,(nRows,nCols),None)
 
         if cornersFound:
             print('corners found')
-            cv.imwrite('chessImages\\'+str(img_index)+'.jpg',imgBGR)
+            cv.imwrite('./chessImages/'+str(img_index)+'.jpg',imgBGR)
             img_index += 1
-            # worldPtsList.append(worldPtsCur)
-            # cornersRefind = cv.cornerSubPix(imgGray,cornersOrg,(11,11),(-1,-1),termCriteria)
-            # imgPtsList.append(cornersRefind)
-            # if ShowPics:
-            #     cv.drawChessboardCorners(imgBGR,(nRows,nCols),cornersRefind,cornersFound)
-            #     cv.imshow("Chessboard",imgBGR)
-            #     cv.waitKey(300)
-        # if cv.waitKey(1) == ord('s'):
-        #     cv.imwrite(img_index+'.jpg')
-        #     img_index += 1
         if cv.waitKey(1) == ord('q'):
             break
     cv.destroyAllWindows()
-    exit()
-
-
-    #Calibrait
-    repError,camMatrix,distCoeff,rvecs,tvecs = cv.calibrateCamera(worldPtsList,imgPtsList,imgGray.shape[::-1],None,None)
-    print("Camera Matrix: \n",camMatrix)
-    print("Reproj Error (pixels): {:.4f}".format(repError))
-
-    #Save Calibration Parameters
-    curFolder = os.path.dirname(os.path.abspath(__file__))
-    paramPath = os.path.join(curFolder,"calibration.npz")
-    np.savez(paramPath,
-             repError=repError,
-             camMatrix=camMatrix,
-             distCoeff=distCoeff,
-             rvecs=rvecs,
-             tvecs=tvecs)
-    return camMatrix,distCoeff
-
-
-def runCalibration():
-    calibrate(ShowPics=False)
+    picam2.stop()
 
 
 if __name__ == '__main__':
-    runCalibration()
+    calibrate()
 
 
