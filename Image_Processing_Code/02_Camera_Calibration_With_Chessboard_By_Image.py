48dc1768fbfeff5632da47f9af64a27de556832e
AmirCodeNinjaX <abolhasaniir1356@gmail.com>
2026-08-02 18:08:01 +0330

    Cleaning The Code

--- Image_Processing_Code/02_Camera_Calibration_With_Chessboard_By_Image.py
+++ Image_Processing_Code/02_Camera_Calibration_With_Chessboard_By_Image.py
@@ -2,9 +2,7 @@ import numpy as np
 import cv2 as cv
 import glob
 import os
-import matplotlib.pyplot as plt
-import configparser
-from Additional._00_Camera_Calibration_Modules import save_camera_config
+from Additional._00_Camera_Calibration_Modules import update_camera_config,CONFIG_PATH
 
 def calibrate(ShowPics=True):
     #ReadImage
@@ -23,15 +21,11 @@ def calibrate(ShowPics=True):
 
     #FindCorners
     for curImgPath in imagePathlist:
-        # print(curImgPath)
         imgBGR = cv.imread(curImgPath)
         imgGray = cv.cvtColor(imgBGR,cv.COLOR_BGR2GRAY)
-        # cv.imshow('bgr image',imgBGR)
-        # cv.imshow('grayscale image',imgGray)
         cornersFound, cornersOrg = cv.findChessboardCorners(imgGray,(nCols,nRows),None)
 
         if cornersFound:
-            # print('corners found')
             worldPtsList.append(worldPtsCur)
             cornersRefind = cv.cornerSubPix(imgGray,cornersOrg,(11,11),(-1,-1),termCriteria)
             imgPtsList.append(cornersRefind)
@@ -42,32 +36,24 @@ def calibrate(ShowPics=True):
         
     cv.destroyAllWindows()
 
-
     #Calibrait
-    repError,camMatrix,distCoeff,rvecs,tvecs = cv.calibrateCamera(worldPtsList,imgPtsList,imgGray.shape[::-1],None,None)
+    repError,camMatrix,distCoeff,_,_ = cv.calibrateCamera(worldPtsList,imgPtsList,imgGray.shape[::-1],None,None)
     print("Camera Matrix: \n",camMatrix)
+    print("Camera Distortion Coeffitiont: \n",distCoeff)
     print("Reproj Error (pixels): {:.4f}".format(repError))
 
     #Save Calibration Parameters
-    curFolder = os.path.dirname(os.path.abspath(__file__))
-    paramPath = os.path.join(curFolder,"Additional\\config.cfg")
-
-    save_camera_config(paramPath,camMatrix,distCoeff)
-
-    # np.savez(paramPath,
-    #          repError=repError,
-    #          camMatrix=camMatrix,
-    #          distCoeff=distCoeff,
-    #          rvecs=rvecs,
-    #          tvecs=tvecs)
-    return camMatrix,distCoeff
+    update_camera_config(CONFIG_PATH,{
+        "intrinsic_matrix":camMatrix,
+        'rows': camMatrix.shape[0],
+        'cols': camMatrix.shape[1],
+        "distortion_coefficients":distCoeff
+    })
 
 
-def runCalibration():
-    calibrate(ShowPics=True)
 
 
 if __name__ == '__main__':
-    runCalibration()
+    calibrate(ShowPics=True)
 
 
--- Image_Processing_Code/04_CameraCalibration_Remove_Distortion.py
+++ Image_Processing_Code/04_CameraCalibration_Remove_Distortion.py
@@ -1,47 +1,25 @@
-import configparser
-import numpy as np
-import io
 import os
 import cv2 as cv
-import matplotlib.pyplot as plt
-from Additional._00_Camera_Calibration_Modules import load_camera_config
-
-
-def RemoveDistotion(camMatirx, dist_Coeff,Camera_Index=0):
-    # root = os.getcwd()
-    # imgPath = os.path.join(root,"DistortionImage//2.jpg")
-    # img = cv.imread(imgPath)
-    cam = cv.VideoCapture(Camera_Index)
-    while True:
-        ret,img = cam.read()
-
-        if(not(ret)): continue
-
-        height, width = img.shape[:2]
-        CameraMatrixNew, _ = cv.getOptimalNewCameraMatrix(camMatirx, dist_Coeff, (width, height), 1, (width, height))
-        imgUndist = cv.undistort(img, camMatirx, dist_Coeff, None, CameraMatrixNew)
-
-        # Draw the line to see Distotion Change
-        # cv.line(img,(1769,103),(1730,922),(255,255,255),2)
-        # cv.line(imgUndist,(1769,103),(1730,922),(255,255,255),2)
-
-
-        plt.figure()
-        plt.subplot(121)
-        plt.imshow(img)
-        plt.subplot(122)
-        plt.imshow(imgUndist)
-        plt.show()
+from Additional._00_Camera_Calibration_Modules import load_camera_config,RemoveDistotion,CONFIG_PATH
 
 def RunRemoveDistotion():
     curFolder = os.path.dirname(os.path.abspath(__file__))
-    paramPath = os.path.join(curFolder,"Additional\\config.cfg")
+    paramPath = os.path.join(curFolder,CONFIG_PATH)
 
-    CamMatrix, dist_Coeff = load_camera_config(paramPath)
+    configs = load_camera_config(paramPath)
+    CamMatrix = configs["intrinsic_matrix"]
+    dist_Coeff = configs["distortion_coefficients"]
+
     print(CamMatrix)
     print(dist_Coeff)
 
-    RemoveDistotion(CamMatrix, dist_Coeff)
+    img = cv.imread('Image Path') # Change This Parameter
+    img = RemoveDistotion(img,CamMatrix,dist_Coeff)
+    while True:
+        cv.imshow("test",img)
+        key = cv.waitKey(1) & 0xFF
+        if key == 27 or key == ord("q"):
+            break
 
 
 if __name__ == '__main__':
